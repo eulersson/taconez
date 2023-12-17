7194c9c470720c6412063446c38f4f06830d4e93
Ramon Blanquer <blanquer.ramon@gmail.com>
2023-12-17 23:39:32 +0100

    Fix whole stack deployment on the Pi.

--- sound-player/docker/run-prod.sh
+++ sound-player/docker/run-prod.sh
@@ -14,6 +14,7 @@ ANESOWA_ROOT=$(echo $(realpath $0) | sed 's|/sound-detector.*||')
 # If launched by the systemd service these two variables will be set.
 ANESOWA_CONTAINER_NAME=${ANESOWA_CONTAINER_NAME:-anesowa-sound-player}
 ANESOWA_VERSION=${ANESOWA_VERSION:-prod}
+PULSEAUDIO_COOKIE=${PULSEAUDIO_COOKIE:-$HOME/.config/pulse/cookie}
 
 if [ "$(uname)" == "Linux" ]; then
   extra_flags="--add-host=host.docker.internal:host-gateway"
@@ -27,6 +28,9 @@ set -x;
 docker run \
   --tty \
   --name $ANESOWA_CONTAINER_NAME \
+  --env PULSE_SERVER=host.docker.internal \
+  --volume $PULSEAUDIO_COOKIE:/root/.config/pulse/cookie \
+  --volume /mnt/nfs/anesowa:/mnt/nfs/anesowa:ro \
   $extra_flags \
   anesowa/sound-player:$ANESOWA_VERSION
 
