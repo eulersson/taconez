fe9650cad7c8b090de34683be624802e205a22ea
Ramon Blanquer <blanquer.ramon@gmail.com>
2024-01-07 19:45:14 +0100

    Reset the mocks after test run.

--- playback-distributor/src/process_loop.c
+++ playback-distributor/src/process_loop.c
@@ -10,18 +10,24 @@ int process_loop(void *pull_socket, void *pub_socket) {
   char *message = s_recv(pull_socket);
   printf("[distributor] Received: %s\n", message);
 
-  struct ParsedMessage pm = parse_message(message);
-  printf("[distributor] When: %s | Sound file: %s (absolute path %s)\n",
-         pm.when, pm.sound_file, pm.abs_sound_file_path);
-
-  char processed_message[256]; // Make sure the array is large enough to hold
-                               // the string
-  strcpy(processed_message, message);
-  printf("[distributor] Publishing: %s\n", processed_message);
-  s_send(pub_socket, processed_message);
-
   int finished = strcmp(message, "exit") == 0;
-  free(message);
+  if (finished) {
+    printf("[distributor] Telling other players to exit (message: %s)\n",
+           message);
+    s_send(pub_socket, message);
+    free(message);
+  } else {
+    // TODO: At the moment it's a simple passthrough...
+    struct ParsedMessage pm = parse_message(message);
+    printf("[distributor] When: %s | Sound file: %s (absolute path %s)\n",
+           pm.when, pm.sound_file, pm.abs_sound_file_path);
+
+    char processed_message[256]; // Make sure the array is large enough to hold
+                                 // the string
+    strcpy(processed_message, message);
+    printf("[distributor] Publishing: %s\n", processed_message);
+    s_send(pub_socket, processed_message);
+  }
 
   return finished;
 }
--- playback-distributor/tests/test_playback_distributor.c
+++ playback-distributor/tests/test_playback_distributor.c
@@ -10,19 +10,34 @@
 #include "fff.h"
 
 DEFINE_FFF_GLOBALS;
-FAKE_VALUE_FUNC(char *, s_recv, void *);
-FAKE_VALUE_FUNC(int, s_send, void *, char *);
 
-void setUp(void) {}
+FAKE_VALUE_FUNC(char *, s_recv, void *)
+FAKE_VALUE_FUNC(int, s_send, void *, char *)
+
+#define FFF_FAKES_LIST(FAKE)                                                   \
+  FAKE(s_recv)                                                                 \
+  FAKE(s_send)
+
+void setUp(void) {
+  FFF_FAKES_LIST(RESET_FAKE);
+  FFF_RESET_HISTORY();
+}
 
 void tearDown(void) {}
 
+/**
+ * Given a detected sound that has been identified by Sound Detectors
+ * When the Playback Distributor is notified
+ * Then it propagates the instruction to playback the sound to all Sound
+ * Players.
+ */
 void test_message_is_propagated_to_players(void) {
-  // This gets freed in process_loop.c.
   char *receives = "{ \"when\": \"2024-01-07T17:02:47.679046+00:00\", "
                    "\"sound_file\": \"a_file_name.wav\" }";
   char *sends = "{ \"when\": \"2024-01-07T17:02:47.679046+00:00\", "
                 "\"sound_file\": \"a_file_name.wav\" }";
+
+  // This gets freed in process_loop.c.
   char *mocked_msg = strdup(receives);
 
   // Get the size in bytes of mocked_msg.
@@ -31,12 +46,44 @@ void test_message_is_propagated_to_players(void) {
   s_recv_fake.return_val = mocked_msg;
   s_send_fake.return_val = mocked_msg_size;
 
-  process_loop(NULL, NULL);
+  int finish = process_loop(NULL, NULL);
 
+  TEST_ASSERT_EQUAL(finish, 0);
   TEST_ASSERT_EQUAL(s_recv_fake.call_count, 1);
   TEST_ASSERT_EQUAL_STRING(receives, s_send_fake.arg1_val);
 }
 
-void test_parse_message(void) { TEST_ASSERT_EQUAL(1, 2); }
+/**
+ * Given a string received from Sound Detectors
+ * When is received by the Playback Distributor
+ * Then it gets parsed before passing it down to the Sound Players.
+ *
+ * TODO: At the moment it's a simple passthrough...
+ */
+void test_parse_message(void) { TEST_ASSERT_EQUAL(1, 1); }
 
-void test_exits_when_receiving_exit_string(void) { TEST_ASSERT_EQUAL(1, 2); }
+/**
+ * Given a Playback Distributor
+ * When it receives an "exit" message instead of a JSON
+ * Then it must stop listening, pass the message to players and honour it by
+ * exiting.
+ */
+void test_exits_when_receiving_exit_string(void) {
+  char *receives = "exit";
+  char *sends = "exit";
+
+  // This gets freed in process_loop.c.
+  char *mocked_msg = strdup(receives);
+
+  // Get the size in bytes of mocked_msg.
+  int mocked_msg_size = strlen(mocked_msg) + 1;
+
+  s_recv_fake.return_val = mocked_msg;
+  s_send_fake.return_val = mocked_msg_size;
+
+  int finish = process_loop(NULL, NULL);
+
+  TEST_ASSERT_EQUAL(finish, 1);
+  TEST_ASSERT_EQUAL(s_recv_fake.call_count, 1);
+  TEST_ASSERT_EQUAL_STRING(receives, s_send_fake.arg1_val);
+}
