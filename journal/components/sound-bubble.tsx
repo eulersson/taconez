08866b44d4220692729b7003d770d558ffe785ed
Ramon Blanquer <blanquer.ramon@gmail.com>
2024-02-16 12:03:25 +0100

    Ability to locally play detected sounds.

--- journal/components/sound-bubble.tsx
+++ journal/components/sound-bubble.tsx
@@ -4,7 +4,7 @@ import { SoundOccurrence } from "@/types";
 export function SoundBubble({ sound }: { sound: SoundOccurrence }) {
   // TODO: See what would happen if `async` was removed from the function signature.
   const onPlayButtonClick = async () => {
-    const audio = new Audio(sound.audio_file_path);
+    const audio = new Audio(`/api/sound/${sound.audio_file_path}`);
     audio.play();
   };
 
