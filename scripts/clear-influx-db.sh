3e20e859d616ec7dfb0e28c7a3e5b957a079bde9
Ramon Blanquer <blanquer.ramon@gmail.com>
2024-06-15 15:39:51 +0200

    Added influx db token in clear-influx-db.sh script

--- scripts/clear-influx-db.sh
+++ scripts/clear-influx-db.sh
@@ -2,7 +2,7 @@
 
 curl -v -X POST \
   'http://localhost:8086/api/v2/delete?org=taconez&bucket=taconez' \
-  -H 'Authorization: Token no_token' \
+  -H 'Authorization: Token vsC0Q_nELv6zi1rjkRKjqVnqdwWoRemazl-zfyWiO1bpfK9ET3WdEL3rAPZVAD_9HtPC0ij7qP7uXgkbKkIiXA==' \
   -d '{
         "start": "1970-01-01T00:00:00Z",
         "stop": "2099-01-01T00:00:00Z",
